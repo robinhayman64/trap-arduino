51455c5c7721cbebc02bff1b7490a3fa740042f3
Kalvin <kalvinmattheus@yahoo.com>
2018-06-23 21:20:20 +0200

    Object mem test

--- main/main.ino
+++ main/main.ino
@@ -2,7 +2,8 @@
 #include "src/dht_library/DHT.h"
 #include "src/gsm_library/GSM.h"
 
-GSM gsm(2, 3);
+//GSM gsm(2, 3);
+GSM *gsm;
 DHT dht(4, 22);
 
 void setup()
@@ -10,16 +11,18 @@ void setup()
 	// Starts the Serial port for debugging
 	Serial.begin(9600);
 
+  gsm = new GSM(2, 3);
+
 	// Starts the temperature sensor
 	dht.begin();
 
 	// Starts the GSM modem
-	if (gsm.start())
+	if (gsm->start())
 	{
 		// Initialize the test post address and message. This may be changed throughout the run of the program in the void loop function.
-		gsm.setAddress("http://erbium.requestcatcher.com/test");
-		gsm.setMessage("Aangeskakel");
-		if (gsm.postRequest())
+		gsm->setAddress("http://erbium.requestcatcher.com/test");
+		gsm->setMessage("Aangeskakel");
+		if (gsm->postRequest())
 		{
 			Serial.println("Startup Successful");
 		}
@@ -33,10 +36,10 @@ void loop()
 
 	// Read the temperature and assign it to the GSM modem
 	float t = dht.readTemperature();
-	gsm.setMessage(String("Die Temperatuur is: ") + String(t));
+	gsm->setMessage(String("Die Temperatuur is: ") + String(t));
 
 	// Use the GSM modem to post the temperature to the server
-	if (gsm.postRequest())
+	if (gsm->postRequest())
 	{
 		Serial.println("Temperature Posted Successfully");
 	}
